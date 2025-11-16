3fdebd3bd1cc9da69c1206bc5782e0bdad876cf1
Dajabre Torain <dptw516@gmail.com>
2025-11-16 14:29:12 -0500

    u

--- components/footer250.php
+++ components/footer250.php
@@ -1,14 +1,16 @@
-    <nav id="footer-navigation">
+    <nav>
       <ul>
-        <li><a href="https://github.com/Sohfahking" target="_blank" title="My GitHub">GitHub</a></li>
-        <li><a href="http://Sohfahking.GitHub.io" target="_blank" title="GitHub Sites Home">GitHub.io</a></li>
-        <li><a href="https://sohfahking.github.io/web250/" target="_blank" title="WEB250 site">WEB250.io</a></li>
-        <li><a href="https://www.freecodecamp.org/DajabreTorain" target="_blank" title="My freeCodeCamp">freeCodeCamp</a>
+        <li><a href="https://github.com/Sohfahking" target="_blank" title="GitHub">GitHub</a></li>
+        <li><a href="http://Sohfahking.GitHub.io" target="_blank" title="GitHub.io">GitHub.io</a></li>
+        <li><a href="https://sohfahking.github.io/web115/" target="_blank" title="WEB115">WEB115.io</a></li>
+        <li><a href="https://sohfahking.github.io/web215/" target="_blank" title="WEB215">WEB215.io</a></li>
+        <li><a href="https://sohfahking.github.io/web250/" target="_blank" title="WEB250">WEB250.io</a></li>
+        <li><a href="https://www.freecodecamp.org/DajabreTorain" target="_blank" title="freeCodeCamp">freeCodeCamp</a>
         </li>
         <li><a href="https://www.codecademy.com/profiles/DajabreTorain" target="_blank"
-            title="My Codecademy">Codecademy</a></li>
-        <li><a href="https://jsfiddle.net/user/DajabreTorain" target="_blank" title="My JSFiddle">JSFiddle</a></li>
-        <li><a href="https://www.linkedin.com/in/dajabre-torain/" target="_blank" title="My LinkedIn">LinkedIn</a></li>
+          title="Codecademy">Codecademy</a></li>
+        <li><a href="https://jsfiddle.net/user/DajabreTorain" target="_blank" title="JSFiddle">JSFiddle</a></li>
+        <li><a href="https://www.linkedin.com/in/dajabre-torain/" target="_blank" title="LinkedIn">LinkedIn</a></li>
       </ul>
     </nav>
     <p>Designed by Dajabre Torain | &copy; 2025</p>
