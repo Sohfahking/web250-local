b5b1258c3f84230aab20042e2eed4675581332cb
Dajabre Torain <dptw516@gmail.com>
2025-12-12 21:37:29 -0500

    hgg

--- components/header250.php
+++ components/header250.php
@@ -1,39 +1,39 @@
 <h1>Dajabre Torain's Daring Tiger | WEB250</h1>
-    <nav>
-      <ul>
-        <li><a href="/index.php?page=index" title="Index">Home</a></li>
-      </ul>
+<nav>
+  <ul>
+    <li><a href="/index.php?page=index" title="Index">Home</a></li>
+  </ul>
 
-      <ul class="main-menu">
-        <li><a href="#" title="Contents">Contents ↓</a>
-          <ul class="sub-menu">
-            <li><a href="/index.php?page=intro250" title="My Introduction">Introduction</a></li>
-            <li><a href="/index.php?page=contract250" title="My Contract">Contract</a></li>
-            <li><a href="/index.php?page=brand250" title="My Brand">Brand</a></li>
-            <li><a href="/index.php?page=template250" title="Temp">Template</a></li>
+  <ul class="main-menu">
+    <li><a href="#" title="Contents">Contents ↓</a>
+      <ul class="sub-menu">
+        <li><a href="/index.php?page=intro250" title="My Introduction">Introduction</a></li>
+        <li><a href="/index.php?page=contract250" title="My Contract">Contract</a></li>
+        <li><a href="/index.php?page=brand250" title="My Brand">Brand</a></li>
+        <li><a href="/index.php?page=template250" title="Temp">Template</a></li>
 
-          </ul>
-        </li>
       </ul>
+    </li>
+  </ul>
 
-      <ul>
-        <li><a href="/carapp/index.php" title="Joy of PHP">My Cars</a></li>
-      </ul>
+  <ul>
+    <li><a href="/carapp/car_inventory.php" title="Joy of PHP">My Cars</a></li>
+  </ul>
 
-      <ul class="main-menu">
-        <li><a href="#" title="Multipage Static PHP">Multipage Sites: Static & PHP ↓</a>
-          <ul class="sub-menu">
-            <li><a href="/multipage_sites/superduper_static/index.htm" title="Multipage Static">SuperDuper Static</a></li>
-            <li><a href="/multipage_sites/superduper_php/index.php" title="Multipage PHP">SuperDuper PHP</a></li>
-          </ul>
-        </li>
+  <ul class="main-menu">
+    <li><a href="#" title="Multipage Static PHP">Multipage Sites: Static & PHP ↓</a>
+      <ul class="sub-menu">
+        <li><a href="/multipage_sites/superduper_static/index.htm" title="Multipage Static">SuperDuper Static</a></li>
+        <li><a href="/multipage_sites/superduper_php/index.php" title="Multipage PHP">SuperDuper PHP</a></li>
       </ul>
+    </li>
+  </ul>
 
-      <ul class="main-menu">
-        <li><a href="/joyofphp/joyphp/src/index.php" title="Joy of PHP">Joy of PHP ↓</a></li>
-        <ul class="sub-menu">
-            <li><a href="/joyofphp/joyphp/src/samsusedcars.php" title="Sam's Used Cars">Sam's Used Cars </a></li>
-          </ul>
-        </li>
+  <ul class="main-menu">
+    <li><a href="/joyofphp/joyphp/src/index.php" title="Joy of PHP">Joy of PHP ↓</a>
+      <ul class="sub-menu">
+        <li><a href="/joyofphp/joyphp/src/samsusedcars.php" title="Sam's Used Cars">Sam's Used Cars </a></li>
       </ul>
-    </nav>
+    </li>
+  </ul>
+</nav>
